bd8f57383aa94f65774668bfe57d196e39cbd334
zendo <p_geondo@snu.ac.kr>
2021-06-16 20:30:24 +0900

    feat: created game page

---
+++ Game.py
@@ -0,0 +1,20 @@
+from tkinter import *
+from tkinter.font import *
+from component import BackButton
+
+
+class Game(Frame):
+    def __init__(self, parent, *args, **kwargs):
+        Frame.__init__(self, parent, *args, **kwargs)
+        self.parent = parent
+        self.difficulty = StringVar()
+
+        self.title = Label(self, textvariable=self.difficulty, font=Font(size=50))
+        self.title.pack()
+
+        self.backButton = BackButton(self)
+        self.backButton.pack()
+
+    def backCommand(self):
+        self.pack_forget()
+        self.parent.Main.pack()
